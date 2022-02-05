68cea902d003287042c9ea0eae7f01a4e0d219bd
efesahin35 <efe.mate@gmail.com>
2022-02-05 20:51:45 +0300

    Create the pages basicly

---
+++ lib/presentation/pages/welcome_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
